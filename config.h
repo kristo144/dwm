718ee2e30720b92d071ffeba5f538ce8744f46dd
Kristiyan Tonev <kris@kristonev.com>
2025-10-27 22:52:11 +0100

    new colorscheme

--- config.h
+++ config.h
@@ -17,12 +17,12 @@ static int smartgaps          = 0;        /* 1 means no outer gap when there is 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
+static char normbgcolor[]           = "#181818";
+static char normbordercolor[]       = "#000000";
+static char normfgcolor[]           = "#227799";
+static char selfgcolor[]            = "#181818";
+static char selbordercolor[]        = "#227799";
+static char selbgcolor[]            = "#227799";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
